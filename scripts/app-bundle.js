e5f9ab20fdf3c76cb515ddda67b3120066574de0
JeroenVinke <jeroenvinke17@gmail.com>
2017-04-05 10:02:32 +0200

    first commit

---
+++ scripts/app-bundle.js
@@ -0,0 +1,95 @@
+define('app',['exports', 'uuid'], function (exports, _uuid) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.App = undefined;
+
+  var uuid = _interopRequireWildcard(_uuid);
+
+  function _interopRequireWildcard(obj) {
+    if (obj && obj.__esModule) {
+      return obj;
+    } else {
+      var newObj = {};
+
+      if (obj != null) {
+        for (var key in obj) {
+          if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
+        }
+      }
+
+      newObj.default = obj;
+      return newObj;
+    }
+  }
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  console.log(uuid.v4());
+
+  var App = exports.App = function App() {
+    _classCallCheck(this, App);
+
+    this.message = 'Hello World!';
+  };
+});
+define('environment',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = {
+    debug: true,
+    testing: true
+  };
+});
+define('main',['exports', './environment'], function (exports, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+
+  var _environment2 = _interopRequireDefault(_environment);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  function configure(aurelia) {
+    aurelia.use.standardConfiguration().feature('resources');
+
+    if (_environment2.default.debug) {
+      aurelia.use.developmentLogging();
+    }
+
+    if (_environment2.default.testing) {
+      aurelia.use.plugin('aurelia-testing');
+    }
+
+    aurelia.start().then(function () {
+      return aurelia.setRoot();
+    });
+  }
+});
+define('resources/index',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+  function configure(config) {}
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template><h1>${message}</h1></template>"; });
+//# sourceMappingURL=app-bundle.js.map
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import * as uuid from 'uuid';
+
+console.log(uuid.v4());
+
+export class App {
+  constructor() {
+    this.message = 'Hello World!';
+  }
+}
